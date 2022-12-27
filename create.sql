779f75aa025f5a516964f127a6c9e5784ed73c9f
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2022-12-27 04:02:36 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -8,14 +8,22 @@ CREATE TABLE genre(
   genre_name varchar(50) NOT NULL
 );
 
+CREATE TABLE viewss(
+  id_views INT NOT NULL,
+  views_v INT NOT NULL,
+  date_first  DATE DEFAULT CURRENT_DATE,
+  date_last  DATE DEFAULT CURRENT_DATE
+);
+
 CREATE TABLE channel(
   ch_name char(50) NOT NULL,
-  ch_views INT NOT NULL,
-  ch_subscribers INT NOT NULL,
+  id_views INT NOT NULL,
   id_genre INT NOT NULL,
   id_country INT NOT NULL
 );
 
+ALTER TABLE channel ADD PRIMARY KEY (ch_name); 
+ALTER TABLE viewss ADD PRIMARY KEY (id_views); 
 ALTER TABLE country ADD PRIMARY KEY (id_country); 
 ALTER TABLE genre ADD PRIMARY KEY (id_genre); 
  
@@ -24,3 +32,7 @@ ALTER TABLE channel ADD CONSTRAINT FK_channel_country FOREIGN KEY (id_country)
 REFERENCES country(id_country);
 ALTER TABLE genre ADD CONSTRAINT FK_channel_genre FOREIGN KEY (id_genre) 
 REFERENCES genre(id_genre);
+ALTER TABLE viewss ADD CONSTRAINT FK_channel_viewss FOREIGN KEY (id_views) 
+REFERENCES viewss(id_views);
+
+
