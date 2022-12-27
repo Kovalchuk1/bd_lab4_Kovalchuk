f644963a26ff5e07a1b6ba0ec2188324385d5c69
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2022-12-27 04:06:25 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,3 +1,8 @@
+CREATE TABLE country(
+  id_country INT NOT NULL,
+  country_name varchar(50) NOT NULL
+);
+
 insert into country(id_country, country_name) values
 (1, 'India'),
 (2, 'US'),
@@ -12,12 +17,28 @@ insert into genre(id_genre, genre_name) values
 (4, 'Games'),
 (5, 'Howto');
 
-insert into channel(ch_name, ch_views,  ch_subscribers, id_genre, id_country) values
-('T-Series', 201929, 225, 1, 1),
-('Cocomelon - Nursery Rhymes', 138918, 143, 2, 2),
-('SET India', 129200, 142.0, 3, 1),
-('PewDiePie', 28525, 111.0, 4, 3),
-('BLACKPINK', 26633, 81.4, 1, 4),
-('5-Minute Crafts', 24134, 77.4, 5, 2),
-('Justin Bieber', 28838, 70.0, 1, 5),
-('Zee TV', 17672, 64.4, 3, 1 );
+insert into viewss(id_views,  views_v, date_first, date_last) values
+( 1, 86234, '2022-09-01', '2022-10-01'),
+( 2, 80342, '2022-09-01', '2022-10-01'),
+( 3, 23443, '2022-09-01', '2022-10-01'),
+( 4, 4567, '2022-09-01', '2022-10-01'),
+( 5, 3456, '2022-09-01', '2022-10-01'),
+(6, 1456, '2022-09-01', '2022-10-01'),
+(7, 978, '2022-09-01', '2022-10-01'),
+( 8, 950,'2022-09-01', '2022-10-01');
+
+insert into channel(ch_name,id_views, id_genre, id_country) values
+('T-Series', 1, 1, 1),
+('Cocomelon - Nursery Rhymes', 2, 2, 2),
+('SET India', 3, 3, 1),
+('PewDiePie', 4, 4, 3),
+('BLACKPINK', 5, 1, 4),
+('5-Minute Crafts', 6, 5, 2),
+('Justin Bieber', 7, 1, 5),
+('Zee TV', 8, 3, 1 );
+
+
+select * from channel
+select * from genre
+select * from country
+select * from viewss
